20cfeac51b33eff057d6b31f136b0f95f740d80f
willemberg bruno <willem.bruno@hotmail.com>
2025-05-01 22:09:29 -0300

    Desafio Forca

---
+++ forca.py
@@ -0,0 +1,102 @@
+palavra = "uva"
+letracerta = [""]*len(palavra)
+tentativa = 6
+tentativacerta=0
+letraerro = 0
+errototal=0
+
+forca = [
+
+    """
+     _______
+    |/      |
+    |       
+    |       
+    |       
+    |      
+    |___
+    """,
+    """
+     _______
+    |/      |
+    |      (_)
+    |       
+    |       
+    |      
+    |___
+    """,
+    """
+     _______
+    |/      |
+    |      (_)
+    |       |
+    |       |
+    |      
+    |___
+    """,
+    """
+     _______
+    |/      |
+    |      (_)
+    |      \\|
+    |       |
+    |      
+    |___
+    """,
+    """
+     _______
+    |/      |
+    |      (_)
+    |      \\|/
+    |       |
+    |      
+    |___
+    """,
+    """
+     _______
+    |/      |
+    |      (_)
+    |      \\|/
+    |       |
+    |      / 
+    |___
+    """,
+    """
+     _______
+    |/      |
+    |      (_)
+    |      \\|/
+    |       |
+    |      / \\
+    |___
+    """
+]
+
+
+
+while errototal < 6 :
+    letra = input("Digite uma letra: ")
+    for i in range(len(palavra)):
+        if letra == palavra[i]:
+           letracerta[i] = palavra[i]
+           tentativacerta+=1
+        else:
+            letraerro+=1
+
+    if len(palavra) == letraerro:
+        errototal+=1
+        tentativa-=1
+    letraerro = 0
+
+    if errototal >= 0 and errototal < 6:
+        print(forca[errototal])
+        print(f"Você ainda tem {tentativa} tentativas")
+    else:
+        print("Tentativas Esgotadas!!")
+
+    print(letracerta)
+    print()
+
+    if tentativacerta == len(letracerta):
+        print("Parabêns Você Acertou a Palavra!!!")
+        break
